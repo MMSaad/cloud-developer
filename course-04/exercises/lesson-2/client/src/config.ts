67e704b995297d01d6c2515226543c6a077ec744
MMSaad <eng.mustafa.muhammad@gmail.com>
2019-12-11 19:53:38 +0300

    startup

--- course-04/exercises/lesson-2/client/src/config.ts
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://1c96tt4712.execute-api.us-east-1.amazonaws.com/dev'
--- course-04/exercises/lesson-2/starter-code/index.js
+++ course-04/exercises/lesson-2/starter-code/index.js
@@ -1,80 +1,39 @@
 'use strict'
 
 const AWS = require('aws-sdk')
+const uuid = require('uuid')
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 const groupsTable = process.env.GROUPS_TABLE
 
 exports.handler = async (event) => {
-  console.log('Processing event: ', event)
 
-  // TODO: Read and parse "limit" and "nextKey" parameters from query parameters
-  // let nextKey // Next key to continue scan operation if necessary
-  // let limit // Maximum number of elements to return
+  const itemId = uuid.v4()
 
-  // HINT: You might find the following method useful to get an incoming parameter value
-  // getQueryParameter(event, 'param')
+  const parseBody = JSON.parse(event.body)
 
-  // TODO: Return 400 error if parameters are invalid
-
-  // Scan operation parameters
-  const scanParams = {
-    TableName: groupsTable,
-    // TODO: Set correct pagination parameters
-    // Limit: ???,
-    // ExclusiveStartKey: ???
+  const item = {
+    id: itemId,
+    ...parseBody
   }
-  console.log('Scan params: ', scanParams)
 
-  const result = await docClient.scan(scanParams).promise()
+ const result = await docClient.put({
+    TableName:groupsTable,
+    Item: item
+  }).promise()
 
-  const items = result.Items
-
-  console.log('Result: ', result)
+if(result.error){
+  throw error
+}
 
   // Return result
   return {
-    statusCode: 200,
+    statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify({
-      items,
-      // Encode the JSON object so a client can return it in a URL as is
-      nextKey: encodeNextKey(result.LastEvaluatedKey)
-    })
+    body: JSON.stringify(item)
   }
 }
 
-/**
- * Get a query parameter or return "undefined"
- *
- * @param {Object} event HTTP event passed to a Lambda function
- * @param {string} name a name of a query parameter to return
- *
- * @returns {string} a value of a query parameter value or "undefined" if a parameter is not defined
- */
-function getQueryParameter(event, name) {
-  const queryParams = event.queryStringParameters
-  if (!queryParams) {
-    return undefined
-  }
-
-  return queryParams[name]
-}
-
-/**
- * Encode last evaluated key using
- *
- * @param {Object} lastEvaluatedKey a JS object that represents last evaluated key
- *
- * @return {string} URI encoded last evaluated key
- */
-function encodeNextKey(lastEvaluatedKey) {
-  if (!lastEvaluatedKey) {
-    return null
-  }
-
-  return encodeURIComponent(JSON.stringify(lastEvaluatedKey))
-}
